5a9483608f0615a3f6f516ea77e38db2070630da
giuzep89 <g.rinaldimusic@gmail.com>
2025-07-15 12:44:54 +0200

    Exercise n.5 completed

---
+++ StatisticsGenerator.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class StatisticsGenerator {
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO Show statistics in the console (n. of words, n. of lines)
+
+        // - Use a while loop to go through the file
+        // - use an index to count the lines
+        // - use the next() method to count the words
+        Scanner fileReader = new Scanner(new File("uitvoer.txt"));
+        int numberOfLines = 0;
+        int numberOfWords = 0;
+
+        while(fileReader.hasNextLine()){
+            String line = fileReader.nextLine();
+            String[] words = line.split(" ");
+            for (String w : words){
+                numberOfWords++;
+            }
+            numberOfLines++;
+        }
+
+        System.out.println("Woorden: " + numberOfWords + "\nRegels: " + numberOfLines);
+        fileReader.close();
+    }
+}
